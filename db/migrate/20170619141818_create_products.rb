6529963e441eaeab94e1dbd69f836909aaa00196
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-19 09:22:57 -0500

    Create scaffolds for categories, products, and brands. Setup associations in Category and Brand models to Products.

---
+++ db/migrate/20170619141818_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :quantity
+      t.text :description
+      t.text :reviews
+      t.references :brand, foreign_key: true
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
