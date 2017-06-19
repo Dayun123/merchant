1a7a4adc9664ec978c26e81a3260f389ea038efe
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-19 10:32:58 -0500

    Add Admin namespace, the weird way

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :brands
-  resources :categories
+  namespace :admin do
+    resources :products
+    resources :brands
+    resources :categories
+  end
+
+  # scope module: 'admin', path: 'admin' do
+  #   resources :products
+  #   resources :brands
+  #   resources :categoriess
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
